a00666d14156de8910d7dd19c76c894490357564
Julien Montmartin <julien.montmartin@fastmail.fm>
2019-05-17 13:20:46 +0200

    Corrections pour Travis CI

--- .travis.el
+++ .travis.el
@@ -14,36 +14,11 @@
                '("MELPA" . "http://melpa.milkbox.net/packages/") t)
   (unless package-archive-contents
     (package-refresh-contents))
-  (dolist (package '(;;ac-capf
-                     ;;ac-helm
-                     ;;auto-complete-config
-                     ;;bm
-                     ;;cargo
-                     ;;cmake-mode
-                     ;;column-marker
-                     ;;epa-file
-                     ;;fill-column-indicator
-                     ;;flycheck-rust
-                     ;;fuzzy
-                     ;;ggtags
-                     ;;graphviz-dot-mode
-                     ;;helm
-                     ;;helm-projectile
+  (dolist (package '(
                      htmlize
-                     ;;idle-highlight-mode
-                     ;;leuven
-                     ;;magit
-                     mmm-mode
-                     ;;ob-rust
-                     ;;projectile
-                     ;;racer
-                     rainbow-delimiters
+					 mmm-mode
+					 rainbow-delimiters
                      rainbow-mode
-                     ;;related
-                     ;;rust-mode
-                     ;;unfill
-                     ;;uniquify
-                     ;;whitespace
 					 ))
       (message "---> %s" package)
       (unless (package-installed-p package)
