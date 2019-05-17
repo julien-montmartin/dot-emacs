1d3c6f9bfe9f0a83b0dee1e11b6128325cb482fc
Julien Montmartin <julien.montmartin@fastmail.fm>
2019-05-17 12:46:26 +0200

    Corrections pour Travis CI

--- .travis.el
+++ .travis.el
@@ -3,36 +3,37 @@
                '("MELPA" . "http://melpa.milkbox.net/packages/") t)
   (unless package-archive-contents
     (package-refresh-contents))
-  (dolist (package '(ac-capf
-                     ac-helm
-                     auto-complete-config
-                     bm
-                     cargo
-                     cmake-mode
-                     column-marker
-                     epa-file
-                     fill-column-indicator
-                     flycheck-rust
-                     fuzzy
-                     ggtags
-                     graphviz-dot-mode
-                     helm
-                     helm-projectile
+  (dolist (package '(;;ac-capf
+                     ;;ac-helm
+                     ;;auto-complete-config
+                     ;;bm
+                     ;;cargo
+                     ;;cmake-mode
+                     ;;column-marker
+                     ;;epa-file
+                     ;;fill-column-indicator
+                     ;;flycheck-rust
+                     ;;fuzzy
+                     ;;ggtags
+                     ;;graphviz-dot-mode
+                     ;;helm
+                     ;;helm-projectile
                      htmlize
-                     idle-highlight-mode
-                     leuven
-                     magit
+                     ;;idle-highlight-mode
+                     ;;leuven
+                     ;;magit
                      mmm-mode
-                     ob-rust
-                     projectile
-                     racer
+                     ;;ob-rust
+                     ;;projectile
+                     ;;racer
                      rainbow-delimiters
                      rainbow-mode
-                     related
-                     rust-mode
-                     unfill
-                     uniquify
-                     whitespace))
+                     ;;related
+                     ;;rust-mode
+                     ;;unfill
+                     ;;uniquify
+                     ;;whitespace
+					 ))
       (message "---> %s" package)
       (unless (package-installed-p package)
         (ignore-errors
